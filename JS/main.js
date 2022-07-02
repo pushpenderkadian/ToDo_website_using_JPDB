f915f65d1a83c11c218b0edff886265fb9c741fe
pushpenderkadian <pushpenderkadian3740@gmail.com>
2022-07-02 20:52:35 +0530

    ToDo website using JPDB

--- JS/main.js
+++ JS/main.js
@@ -146,7 +146,7 @@ function showTask() {
               <div class="modal-dialog">
                 <div class="modal-content">
                   <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Update Task</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                   </div>
                   <div class="modal-body">
